a4b288c18752c2859bcd72f39d14c3562cbe79b0
Alva Lin <asjun2lin@outlook.com>
2023-11-15 23:43:30 +0800

    添加 web 项目

---
+++ zs-web/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from 'antd';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Button type="primary" >Button</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
